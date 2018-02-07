931479ee22f8301efc4ca5354163428d4bdd0662
Brandon Covington <brandoncovington@Interapts-MacBook-Pro.local>
2018-02-07 13:27:02 -0500

    Initial Commit

---
+++ assesment2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  assesment2
+//
+//  Created by Brandon Covington on 2/7/18.
+//  Copyright © 2018 Brandon Covington. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
